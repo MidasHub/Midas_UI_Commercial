925b00cb3746db56590d3ad2eaad438cc21485df
Jean <51810762+Anhjean@users.noreply.github.com>
2020-11-07 10:40:54 +0700

    translate toolbar and set Permission to toolbar
    - Translate toolbar objs
    - add directive module into core.module
    - set persion "ALL_FUnction" to all menu on toolbar expect Institution menu

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -29,6 +29,7 @@ import { RouteReusableStrategy } from './route/route-reusable-strategy';
 import { SharedModule } from '../shared/shared.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { SearchModule } from 'app/search/search.module';
+import { DirectivesModule } from '../directives/directives.module';
 
 /** Custom Components */
 import { ShellComponent } from './shell/shell.component';
@@ -45,6 +46,7 @@ import { ContentComponent } from './shell/content/content.component';
 @NgModule({
   imports: [
     SharedModule,
+    DirectivesModule,
     HttpClientModule,
     RouterModule
   ],
