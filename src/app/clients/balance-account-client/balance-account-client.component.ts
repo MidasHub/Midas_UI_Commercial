6514b4d895be4509896ed47616efd433aba6c75b
Dang Khoa <khoadang200598@gmail.com>
2020-11-24 00:08:46 +0700

    add Danh sách công nợ phải thu

--- src/app/clients/balance-account-client/balance-account-client.component.ts
+++ src/app/clients/balance-account-client/balance-account-client.component.ts
@@ -7,6 +7,7 @@ import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {animate, state, style, transition, trigger} from '@angular/animations';
 import {AccountingService} from '../../accounting/accounting.service';
+import {AuthenticationService} from '../../core/authentication/authentication.service';
 
 @Component({
   selector: 'midas-balance-account-client',
@@ -22,21 +23,31 @@ import {AccountingService} from '../../accounting/accounting.service';
 })
 export class BalanceAccountClientComponent implements OnInit {
   expandedElement: any;
+  expandedElement2: any;
   displayedColumns: any[] = ['customer_name', 'staff_name', 'account_no', 'account_balance_derived'];
+  displayedColumns2: any[] = ['customer_name', 'staff_name', 'account_no', 'account_balance_derived'];
   dataSource: any[] = [];
+  dataSource2: any[] = [];
   formFilter: FormGroup;
   accountFilter: any[] = [];
   accountsShow: any[] = [];
   staffs: any[] = [];
+  currentUser: any;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor(
     private clientServices: ClientsService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private authenticationService: AuthenticationService,
   ) {
+    this.currentUser = this.authenticationService.getCredentials();
     this.clientServices.getBalanceAccountOfCustomer().subscribe(result => {
-      this.dataSource = result?.result?.listBalanceCustomer;
+      const {permissions, staffId} = this.currentUser;
+      console.log(permissions, result);
+      const permit_userTeller = permissions.includes('TXNOFFICE_CREATE');
+      this.dataSource = result?.result?.listBalanceCustomer.filter((v: any) => Number(v.account_balance_derived) >= 0 && (permit_userTeller || staffId === v.staff_id));
+      this.dataSource2 = result?.result?.listBalanceCustomer.filter((v: any) => Number(v.account_balance_derived) < 0 && (permit_userTeller || staffId === v.staff_id));
       this.accountFilter = this.dataSource;
       this.loadData();
     });
@@ -57,11 +68,16 @@ export class BalanceAccountClientComponent implements OnInit {
     this.accountFilter = this.accountFilter.sort((a, b) => {
       const isAsc = sort.direction === 'asc';
       switch (sort.active) {
-        case 'customer_name': return this.compare(a.customer_name, b.customer_name, isAsc);
-        case 'staff_name': return this.compare(a.staff_name, b.staff_name, isAsc);
-        case 'account_no': return this.compare(a.account_no, b.account_no, isAsc);
-        case 'account_balance_derived': return this.compare(a.account_balance_derived, b.account_balance_derived, isAsc);
-        default: return 0;
+        case 'customer_name':
+          return this.compare(a.customer_name, b.customer_name, isAsc);
+        case 'staff_name':
+          return this.compare(a.staff_name, b.staff_name, isAsc);
+        case 'account_no':
+          return this.compare(a.account_no, b.account_no, isAsc);
+        case 'account_balance_derived':
+          return this.compare(a.account_balance_derived, b.account_balance_derived, isAsc);
+        default:
+          return 0;
       }
     });
     this.paginator.pageIndex = 0;
@@ -83,9 +99,11 @@ export class BalanceAccountClientComponent implements OnInit {
       this.filterData()
     ]);
   }
+
   compare(a: number | string, b: number | string, isAsc: boolean) {
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   }
+
   loadData() {
     const pageIndex = this.paginator.pageIndex;
     const pageSize = this.paginator.pageSize;
