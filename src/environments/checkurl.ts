bd68f7ff14368390ef3bdc3b531fd1b95905d8be
Dang Khoa <khoadang200598@gmail.com>
2020-12-16 22:06:50 +0700

    save code

--- src/environments/checkurl.ts
+++ src/environments/checkurl.ts
@@ -1,34 +1,33 @@
-
-console.log('subdomain',window.location.hostname.split(".")[0]);
-const subdomain = window.location.hostname.split(".")[0];
-let tenant= 'jean';
-let billpostenant = 'jean'
-let defaultbaseURL = 'https://uat.tekcompay.com:9443'
-let defaultbillposURL = 'https://uat.tekcompay.com:8287'
-let apiProvider ='/midas/api'
-let apiVersion = '/v1'
+console.log('subdomain', window.location.hostname.split('.')[0]);
+const subdomain = window.location.hostname.split('.')[0];
+let tenant = 'jean';
+let billpostenant = 'jean';
+let defaultbaseURL = 'http://uat.tekcompay.com:9443';
+let defaultbillposURL = 'http://uat.tekcompay.com:8287';
+let apiProvider = '/midas/api';
+let apiVersion = '/v1';
 
 
 if (subdomain === 'staging-hn') {
-  tenant = 'hanoi'
-  billpostenant = 'hanoi'
+  tenant = 'hanoi';
+  billpostenant = 'hanoi';
 }
 
 if (subdomain === 'hanoi') {
-  tenant = 'hanoi'
-  billpostenant = 'hanoi'
-  defaultbaseURL = 'https://app2.midascore.net:8443'
-  defaultbillposURL= 'https://app1.midascore.net:8088'
-  apiProvider ='/fineract-provider/api'
+  tenant = 'hanoi';
+  billpostenant = 'hanoi';
+  defaultbaseURL = 'https://app2.midascore.net:8443';
+  defaultbillposURL = 'https://app1.midascore.net:8088';
+  apiProvider = '/fineract-provider/api';
 
 }
 
 export default {
-    coreT: tenant,
-    billposT: billpostenant,
-    defaultbaseURL: defaultbaseURL,
-    defaultbillposURL:defaultbillposURL,
-    apiProvider:apiProvider,
-    apiVersion:apiVersion
+  coreT: tenant,
+  billposT: billpostenant,
+  defaultbaseURL: defaultbaseURL,
+  defaultbillposURL: defaultbillposURL,
+  apiProvider: apiProvider,
+  apiVersion: apiVersion
 
-}
+};
