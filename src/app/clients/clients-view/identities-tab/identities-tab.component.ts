fad922b445088a9ed2adb5ae7898d8c959942ece
ttsang96 <taitatsang@gmail.com>
2021-03-19 10:28:03 +0700

    remove un use code
    remove un use code

--- src/app/clients/clients-view/identities-tab/identities-tab.component.ts
+++ src/app/clients/clients-view/identities-tab/identities-tab.component.ts
@@ -22,7 +22,6 @@ import { analyzeAndValidateNgModules } from "@angular/compiler";
 import { AddIdentitiesExtraInfoComponent } from "./dialog-add-identities-extra-info/add-identities-extra-info.component";
 import { BanksService } from "../../../banks/banks.service";
 
-import { OwlOptions } from 'ngx-owl-carousel-o';
 import { Logger } from "app/core/logger/logger.service";
 const log = new Logger("-IDENTIFIER TAB-");
 
@@ -35,31 +34,6 @@ const log = new Logger("-IDENTIFIER TAB-");
   styleUrls: ["./identities-tab.component.scss"],
 })
 export class IdentitiesTabComponent {
-
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
-    navSpeed: 700,
-    navText: ['', ''],
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 4
-      }
-    },
-    nav: true
-  }
 
 
   searchKey: string;
