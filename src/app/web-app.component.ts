3820636053034b76115a97c6bd8dc20281e3bb82
ttsang96 <taitatsang@gmail.com>
2021-04-08 12:32:47 +0700

    fix alert show time from 5s to 10s
    fix alert show time from 5s to 10s

--- src/app/web-app.component.ts
+++ src/app/web-app.component.ts
@@ -154,7 +154,7 @@ export class WebAppComponent implements OnInit {
         `${alertEvent.message}`,
         this.i18nService.getTranslate("Client_Component.ClientStepper.lblClose"),
         {
-          duration: alertEvent.msgDuration ? alertEvent.msgDuration : 5000,
+          duration: alertEvent.msgDuration ? alertEvent.msgDuration : 10000,
           horizontalPosition: alertEvent.hPosition ? alertEvent.hPosition : "right",
           verticalPosition: alertEvent.vPosition ? alertEvent.vPosition : "top",
           panelClass: [alertEvent.msgClass],
