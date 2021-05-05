4b12f199b08302368f313f4435cfdfeece89b2ed
ttsang96 <taitatsang@gmail.com>
2021-05-06 00:18:49 +0700

    fix reload in make fee advance
    fix reload in make fee advance

--- src/app/transactions/dialog/make-fee-on-advance/make-fee-on-advance.component.ts
+++ src/app/transactions/dialog/make-fee-on-advance/make-fee-on-advance.component.ts
@@ -90,7 +90,7 @@ export class MakeFeeOnAdvanceComponent implements OnInit {
       if (data) {
         this.savingsService.makeFeeOnAdvanceExecute(formData).subscribe((result: any) => {
           const message = "Ứng tiền thành công";
-          this.savingsService.handleResponseApiSavingTransaction(result, message, true);
+          this.savingsService.handleResponseApiSavingTransaction(result, message, false);
           return this.dialogRef.close({ status: true });
         });
       }
