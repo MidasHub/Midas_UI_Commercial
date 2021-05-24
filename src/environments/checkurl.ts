e4a21390a6f7e925575fe5a21475046b6e4a14bd
ttsang96 <taitatsang@gmail.com>
2021-05-24 12:51:56 +0700

    fix dev env

--- src/environments/checkurl.ts
+++ src/environments/checkurl.ts
@@ -3,25 +3,12 @@ const subdomain = window.location.hostname.split(".")[0];
 let tenant = "default";
 let billpostenant = "default";
 let ictenant = "default";
-// let defaultIcURL = "https://ic.kiotthe.com";
 // let defaultIcURL = "http://localhost:8088";
 let defaultIcURL = "https://icdev.kiotthe.com";
 
 let defaultbaseURL = "https://icdev.kiotthe.com";
 // let defaultbillposURL = "http://localhost:8088";
 let defaultbillposURL = "https://icdev.kiotthe.com";
-
-// prod 160
-// let defaultbaseURL = "https://api.kiotthe.com";
-// let defaultbillposURL = "https://api.kiotthe.com";
-
-// prod TEKCOMPAY, HDCREDIT
-// let defaultbaseURL = "https://midas.kiotthe.com";
-// let defaultbillposURL = "https://midas.kiotthe.com";
-
-// prod local
-// let defaultbaseURL = "https://midas.kiotthe.com";
-// let defaultbillposURL = "http://localhost:8088";
 
 let apiProvider = "/midas/api";
 const apiVersion = "/v1";
