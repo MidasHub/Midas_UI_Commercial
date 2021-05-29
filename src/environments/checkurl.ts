795eb45f5b2939f19c516dfe9f29e362fceeb22f
ttsang96 <taitatsang@gmail.com>
2021-05-29 13:29:55 +0700

    fix evn

--- src/environments/checkurl.ts
+++ src/environments/checkurl.ts
@@ -5,10 +5,6 @@ let billpostenant = "default";
 let ictenant = "default";
 let defaultIcURL = "https://ic.kiotthe.com";
 
-// prod 160
-// let defaultbaseURL = "https://api.kiotthe.com";
-// let defaultbillposURL = "https://api.kiotthe.com";
-
 // prod TEKCOMPAY, HDCREDIT
 let defaultbaseURL = "https://midas.kiotthe.com";
 let defaultbillposURL = "https://midas.kiotthe.com";
