1bf19afebcfd6572f4ef2b7844417dd157e2d7e7
ttsang96 <taitatsang@gmail.com>
2021-06-10 07:50:43 +0700

    change payment type thu/chi hộ

--- src/app/savings/savings-account-view/form-dialog/advance/advance.component.ts
+++ src/app/savings/savings-account-view/form-dialog/advance/advance.component.ts
@@ -41,11 +41,11 @@ export class AdvanceComponent implements OnInit {
       value: "Ứng tiền phí",
     },
     {
-      id: "3",
+      id: "58",
       value: "Thu hộ",
     },
     {
-      id: "37",
+      id: "58",
       value: "Chi hộ",
     },
   ];
