572bb06b84528e267b070dcc08d4b067a0e09f23
ttsang96 <taitatsang@gmail.com>
2021-06-12 01:24:17 +0700

    fix status identifer

--- src/app/clients/clients-view/identities-tab/identities-tab.component.ts
+++ src/app/clients/clients-view/identities-tab/identities-tab.component.ts
@@ -23,6 +23,7 @@ import { AddIdentitiesExtraInfoComponent } from "./dialog-add-identities-extra-i
 import { BanksService } from "../../../banks/banks.service";
 
 import { Logger } from "app/core/logger/logger.service";
+import { ConfirmDialogComponent } from "app/transactions/dialog/confirm-dialog/confirm-dialog.component";
 const log = new Logger("-IDENTIFIER TAB-");
 
 /**
@@ -488,20 +489,24 @@ export class IdentitiesTabComponent {
     });
   }
 
-  onToggleCardStatus(e:any, identifyId: string){
-    this.clientService.updateStatusIdentify(identifyId, e.checked); 
-    if(confirm("Are you sure ?")) {
-      this.clientService.updateStatusIdentify(identifyId, e.checked).subscribe((res: any) => {
-        console.log(res);
-        if (res.result.Row_affected === 1) {
-          this.alertService.alert({ message: "Thực hiện thành công", msgClass: "cssInfo" });
-        }
-     });
-    }
-
-    
-
-
-
+  onToggleCardStatus(e: any, identifyId: string) {
+    // this.clientService.updateStatusIdentify(identifyId, e.checked);
+    const dialog = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        message: `Xác nhận ${e.checked ? 'giữ thẻ' : 'bỏ giữ thẻ'}`,
+        title: "Trạng thái giữ thẻ",
+      },
+    });
+    dialog.afterClosed().subscribe((data) => {
+      if (data) {
+        this.clientService.updateStatusIdentify(identifyId, e.checked).subscribe((res: any) => {
+          if (res.result.Row_affected === 1) {
+            this.alertService.alert({ message: "Thực hiện thành công", msgClass: "cssInfo" });
+          }
+        });
+      } else {
+        location.reload()
+       }
+    });
   }
 }
