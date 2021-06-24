f69bdf507831444fcd45bc5d4b8c3441fcb8275d
ttsang96 <taitatsang@gmail.com>
2021-06-24 19:34:37 +0700

    fix add card
    - add is valid base on luhn algo, fix validate card number,

--- src/app/clients/clients-view/identities-tab/identities-tab.component.ts
+++ src/app/clients/clients-view/identities-tab/identities-tab.component.ts
@@ -235,6 +235,7 @@ export class IdentitiesTabComponent {
           status,
           limitCard,
           classCard,
+          isValid,
         } = response.data.value;
         const documentTypes = response.documentTypes;
         const document = documentTypes.find((v: any) => v.id === documentTypeId);
@@ -303,6 +304,7 @@ export class IdentitiesTabComponent {
                   expireDate: expiredDate,
                   limitCard: limitCard,
                   classCard: classCard,
+                  isValid: isValid,
                 })
                 .subscribe((res2: any) => {
                   return call_return();
