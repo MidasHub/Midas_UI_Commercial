9a27dc39dc181552563e0bde6b218e86454197d8
ttsang96 <taitatsang@gmail.com>
2021-07-28 14:24:46 +0700

    fix update card on current time filter

--- src/app/transactions/rollTerm-schedule-transaction/due-day-card-tab/due-day-card-tab.component.ts
+++ src/app/transactions/rollTerm-schedule-transaction/due-day-card-tab/due-day-card-tab.component.ts
@@ -106,7 +106,6 @@ export class DueDayCardTabComponent implements OnInit {
       stageFilter: ["ALL"],
       query: [""],
       viewDoneTransaction: [false],
-
     });
   }
 
@@ -205,7 +204,7 @@ export class DueDayCardTabComponent implements OnInit {
   }
 
   submitTransactionRollTerm(card: any, rollTermBooking: any, requestAmount: any, feeRate: any) {
-    const info: any = {};
+    let info: any = {};
     rollTermBooking.forEach((booking: any) => {
       booking.amountBooking = booking.amountBooking;
       booking.txnDate = this.datePipe.transform(booking.txnDate, "dd/MM/yyyy");
@@ -347,7 +346,12 @@ export class DueDayCardTabComponent implements OnInit {
   }
 
   updateCardTrackingState(index: number) {
-    const updateData = this.dataSource[index];
+    let updateData = this.dataSource[index];
+
+    let fromDate = this.formDate.get("fromDate").value;
+    updateData.month = this.datePipe.transform(fromDate, "MM");
+    updateData.year = this.datePipe.transform(fromDate, "yyyy");
+
     this.isLoading = true;
     this.transactionService.updateCardTrackingState(updateData).subscribe((result) => {
       this.isLoading = false;
