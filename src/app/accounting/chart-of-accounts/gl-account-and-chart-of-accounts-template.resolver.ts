7d9f110a0ea2b5ae94f701a2836d7b3b710a4595
Jean <anh.nh@outlook.com>
2021-07-30 21:16:54 +0700

    fix chart of account component
    fix chart of account (COA) component:
      - wrong logic check that make the app can't create tree view of COA
      - wrong logic check that make the app can show the account detail

--- src/app/accounting/chart-of-accounts/gl-account-and-chart-of-accounts-template.resolver.ts
+++ src/app/accounting/chart-of-accounts/gl-account-and-chart-of-accounts-template.resolver.ts
@@ -1,20 +1,19 @@
 /** Angular Imports */
-import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { Resolve, ActivatedRouteSnapshot } from "@angular/router";
 
 /** rxjs Imports */
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 /** Custom Services */
-import { AccountingService } from '../accounting.service';
+import { AccountingService } from "../accounting.service";
 
 /**
  * GL Account and chart of accounts template data resolver.
  */
 @Injectable()
 export class GlAccountAndChartOfAccountsTemplateResolver implements Resolve<Object> {
-
   /**
    * @param {AccountingService} accountingService Accounting service.
    */
@@ -25,29 +24,41 @@ export class GlAccountAndChartOfAccountsTemplateResolver implements Resolve<Obje
    * @returns {Observable<any>}
    */
   resolve(route: ActivatedRouteSnapshot): Observable<any> {
-    const id = route.paramMap.get('id');
+    const id = route.paramMap.get("id");
 
     return this.accountingService.getGlAccount(id, true).pipe(
       map((glAccountData: any) => {
+        // console.log ('account template:', glAccountData);
+        /** logic chỗ này như sau
+         * Nếu 1 gl account có header thì accountOptions sẽ nhận gía trị dòng switch bên dưới
+         * Nếu GL Accoung ko có header se gây lỗi ở hàm accountOptions.find((accountOption: any)
+         * Nên cần check logic nếu accountOptions là mảng rỗng thi ko gọi hàm này
+         */
         let accountOptions = [];
         switch (glAccountData.type.value) {
-          case 'ASSET': accountOptions = glAccountData.assetHeaderAccountOptions;
-          break;
-          case 'EQUITY': accountOptions = glAccountData.equityHeaderAccountOptions;
-          break;
-          case 'EXPENSE': accountOptions = glAccountData.expenseHeaderAccountOptions;
-          break;
-          case 'INCOME': accountOptions = glAccountData.incomeHeaderAccountOptions;
-          break;
-          case 'LIABILITY': accountOptions = glAccountData.liabilityHeaderAccountOptions;
-          break;
+          case "ASSET":
+            accountOptions = glAccountData.assetHeaderAccountOptions;
+            break;
+          case "EQUITY":
+            accountOptions = glAccountData.equityHeaderAccountOptions;
+            break;
+          case "EXPENSE":
+            accountOptions = glAccountData.expenseHeaderAccountOptions;
+            break;
+          case "INCOME":
+            accountOptions = glAccountData.incomeHeaderAccountOptions;
+            break;
+          case "LIABILITY":
+            accountOptions = glAccountData.liabilityHeaderAccountOptions;
+            break;
         }
-        glAccountData.parent = accountOptions.find((accountOption: any) => {
-          return accountOption.id === glAccountData.parentId;
-        });
+        if (accountOptions !== []) {
+          glAccountData.parent = accountOptions.find((accountOption: any) => {
+            return accountOption.id === glAccountData.parentId;
+          });
+        }
         return glAccountData;
       })
     );
   }
-
 }
