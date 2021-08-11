f48bdaac3beaafe018612c5f286885e2f01a8e49
ttsang96 <taitatsang@gmail.com>
2021-08-11 09:22:32 +0700

    hide undo saving transaction on teller role

--- src/app/savings/savings-account-view/transactions/view-transaction/view-transaction.component.ts
+++ src/app/savings/savings-account-view/transactions/view-transaction/view-transaction.component.ts
@@ -1,28 +1,27 @@
 /** Angular Imports */
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MatDialog } from '@angular/material/dialog';
-import { DatePipe } from '@angular/common';
+import { Component } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { MatDialog } from "@angular/material/dialog";
+import { DatePipe } from "@angular/common";
 
 /** Custom Services */
-import { SavingsService } from 'app/savings/savings.service';
-import { SettingsService } from 'app/settings/settings.service';
+import { SavingsService } from "app/savings/savings.service";
+import { SettingsService } from "app/settings/settings.service";
 
 /** Custom Dialogs */
-import { UndoTransactionDialogComponent } from '../../custom-dialogs/undo-transaction-dialog/undo-transaction-dialog.component';
-import { AlertService } from 'app/core/alert/alert.service';
+import { UndoTransactionDialogComponent } from "../../custom-dialogs/undo-transaction-dialog/undo-transaction-dialog.component";
+import { AlertService } from "app/core/alert/alert.service";
 
 /**
  * View Transaction Component.
  * TODO: Add support for account transfers.
  */
 @Component({
-  selector: 'mifosx-view-transaction',
-  templateUrl: './view-transaction.component.html',
-  styleUrls: ['./view-transaction.component.scss']
+  selector: "mifosx-view-transaction",
+  templateUrl: "./view-transaction.component.html",
+  styleUrls: ["./view-transaction.component.scss"],
 })
 export class ViewTransactionComponent {
-
   /** Transaction data. */
   transactionData: any;
 
@@ -35,63 +34,64 @@ export class ViewTransactionComponent {
    * @param {DatePipe} datePipe DatePipe.
    * @param {SettingsService} settingsService Setting service
    */
-  constructor(private savingsService: SavingsService,
-              private datePipe: DatePipe,
-              private alertService: AlertService,
-              private route: ActivatedRoute,
-              private router: Router,
-              public dialog: MatDialog,
-              private settingsService: SettingsService) {
+  constructor(
+    private savingsService: SavingsService,
+    private datePipe: DatePipe,
+    private alertService: AlertService,
+    private route: ActivatedRoute,
+    private router: Router,
+    public dialog: MatDialog,
+    private settingsService: SettingsService
+  ) {
     this.route.data.subscribe((data: { savingsAccountTransaction: any }) => {
       this.transactionData = data.savingsAccountTransaction;
     });
-
   }
 
   /**
    * Undo the savings transaction
    */
   undoTransaction() {
+    this.savingsService.checkValidRevertSavingTransaction(this.transactionData.id).subscribe((data) => {
+      if (!data.result.isValid) {
+        this.alertService.alert({
+          message: `${data.result.message}`,
+          msgClass: "cssDanger",
+          hPosition: "center",
+        });
 
-    this.savingsService.checkValidRevertSavingTransaction(this.transactionData.id).subscribe(data => {
-        if (!data.result.isValid) {
-          this.alertService.alert({
-            message: `${data.result.message}`,
-            msgClass: "cssDanger",
-            hPosition: "center",
-          });
+        return;
+      } else {
+        const accountId = this.route.parent.snapshot.params["savingAccountId"];
+        const undoTransactionAccountDialogRef = this.dialog.open(UndoTransactionDialogComponent);
+        undoTransactionAccountDialogRef.afterClosed().subscribe((response: any) => {
+          if (response.confirm) {
+            const locale = this.settingsService.language.code;
+            const dateFormat = this.settingsService.dateFormat;
+            const data = {
+              transactionDate: this.datePipe.transform(
+                this.transactionData.date && new Date(this.transactionData.date),
+                dateFormat
+              ),
+              transactionAmount: 0,
+              dateFormat,
+              locale,
+            };
+            this.savingsService
+              .executeSavingsAccountTransactionsCommand(accountId, "undo", data, this.transactionData.id)
+              .subscribe(() => {
+                this.alertService.alert({
+                  message: `Hủy giao dịch ${this.transactionData.id} thành công!`,
+                  msgClass: "cssSuccess",
+                  hPosition: "center",
+                });
+                this.router.navigate(["../"], { relativeTo: this.route.parent });
 
-          return;
-        }else{
-          const accountId = this.route.parent.snapshot.params['savingAccountId'];
-    const undoTransactionAccountDialogRef = this.dialog.open(UndoTransactionDialogComponent);
-    undoTransactionAccountDialogRef.afterClosed().subscribe((response: any) => {
-      if (response.confirm) {
-        const locale = this.settingsService.language.code;
-        const dateFormat = this.settingsService.dateFormat;
-        const data = {
-          transactionDate: this.datePipe.transform(this.transactionData.date && new Date(this.transactionData.date), dateFormat),
-          transactionAmount: 0,
-          dateFormat,
-          locale
-        };
-        this.savingsService.executeSavingsAccountTransactionsCommand(accountId, 'undo', data, this.transactionData.id).subscribe(() => {
-          this.alertService.alert({
-            message: `Hủy giao dịch ${this.transactionData.id} thành công!`,
-            msgClass: "cssSuccess",
-            hPosition: "center",
-          });
-          this.router.navigate(['../'], { relativeTo: this.route.parent });
-
-          // this.router.navigate(['../../transactions'], { relativeTo: this.route });
-
+                // this.router.navigate(['../../transactions'], { relativeTo: this.route });
+              });
+          }
         });
       }
     });
-        }
-    })
-
-
   }
-
 }
