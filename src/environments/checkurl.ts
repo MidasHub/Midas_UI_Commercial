78d94ccd9874c132085ebfdf7d7639675a18cb56
ttsang96 <taitatsang@gmail.com>
2021-09-21 10:47:51 +0700

    change url domain from .com to .app

--- src/environments/checkurl.ts
+++ src/environments/checkurl.ts
@@ -3,17 +3,17 @@ const subdomain = window.location.hostname.split('.')[0];
 let tenant = "default";
 let billpostenant = "default";
 let ictenant = "default";
-let defaultIcURL = "https://ic.kiotthe.com";
+let defaultIcURL = "https://ic.kiotthe.app";
 
 // prod 160
-let defaultbaseURL = "https://api.kiotthe.com";
-let defaultbillposURL = "https://api.kiotthe.com";
+let defaultbaseURL = "https://api.kiotthe.app";
+let defaultbillposURL = "https://api.kiotthe.app";
 
 const apiProvider = '/midas/api';
 const apiVersion = '/v1';
 
 let defaultNotificationURL = "https://staging.kiotthe.com";
-let documentURL = "https://document.kiotthe.com";
+let documentURL = "https://document.kiotthe.app";
 export default {
   coreT: tenant,
   billposT: billpostenant,
