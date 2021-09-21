b843ad4e7b66fc1a392baa1a59ce713db3549052
ttsang96 <taitatsang@gmail.com>
2021-09-21 11:47:42 +0700

    change from domain .com to .app

--- src/environments/checkurl.ts
+++ src/environments/checkurl.ts
@@ -3,17 +3,17 @@ const subdomain = window.location.hostname.split(".")[0];
 let tenant = "default";
 let billpostenant = "default";
 let ictenant = "default";
-let defaultIcURL = "https://ic.kiotthe.com";
+let defaultIcURL = "https://ic.kiotthe.app";
 
 // prod TEKCOMPAY, HDCREDIT
-let defaultbaseURL = "https://midas.kiotthe.com";
-let defaultbillposURL = "https://midas.kiotthe.com";
+let defaultbaseURL = "https://midas.kiotthe.app";
+let defaultbillposURL = "https://midas.kiotthe.app";
 
 let apiProvider = "/midas/api";
 const apiVersion = "/v1";
 
 let defaultNotificationURL = "https://staging.kiotthe.com";
-let documentURL = "https://document.kiotthe.com";
+let documentURL = "https://document.kiotthe.app";
 export default {
   coreT: tenant,
   billposT: billpostenant,
