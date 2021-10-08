380163248ae1087ed82fb8e7c2b6e36353c414dd
ttsang96 <taitatsang@gmail.com>
2021-10-09 01:55:34 +0700

    add. money pipe

---
+++ src/app/pipes/money.pipe.ts
@@ -0,0 +1,12 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'money'
+})
+export class MoneyPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return String(value ? value : 0).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
+}
